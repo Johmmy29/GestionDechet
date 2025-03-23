93ea4f4870371e408b64747159123afd9b83310b
marzaguina <133380267+marzaguina@users.noreply.github.com>
2025-03-23 18:02:44 +0100

    Update Couleur.java

--- gestionDechet/src/gestionDechet/Couleur.java
+++ gestionDechet/src/gestionDechet/Couleur.java
@@ -1,10 +1,11 @@
+package gestionDechet;
 
 /**
- * 
+ * Enumération des différentes couleurs de poubelles
  */
 public enum Couleur {
-    VERT,
-    BLEUE,
-    NOIR,
-    JAUNE
+    VERT,   // Poubelle pour les déchets en verre
+    BLEUE,  // Poubelle pour les papiers
+    NOIR,   // Poubelle pour les autres déchets
+    JAUNE   // Poubelle pour les plastiques, cartons, canettes, conserves
 }
