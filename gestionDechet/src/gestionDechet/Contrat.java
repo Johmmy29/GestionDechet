41aef2fbc243f4812075c9b45330ee9a456956d2
marzaguina <133380267+marzaguina@users.noreply.github.com>
2025-03-23 18:02:04 +0100

    Update Contrat.java

--- gestionDechet/src/gestionDechet/Contrat.java
+++ gestionDechet/src/gestionDechet/Contrat.java
@@ -1,65 +1,82 @@
+package gestionDechet;
 
-import java.util.*;
+import java.time.LocalDateTime;
 
-/**
- * 
- */
 public class Contrat {
 
-    /**
-     * Default constructor
-     */
-    public Contrat() {
-    }
-
-    /**
-     * 
-     */
-    private string idContrat;
-
-    /**
-     * 
-     */
-    private Date dateDebut;
-
-    /**
-     * 
-     */
-    private Date dateFin;
-
-    /**
-     * 
-     */
+    private int idContrat;
+    private LocalDateTime dateDebut;
+    private LocalDateTime dateFin;
     private String reglesUtilisation;
+    private int idCommerce;
 
-    /**
-     * 
-     */
-    private string commerce;
-
-    /**
-     * @param nouvelleDateFin 
-     * @return
-     */
-    public void renouvelerContrat(Datetime nouvelleDateFin) {
-        // TODO implement here
-        return null;
+    // Constructeur pour créer un contrat entre le centre de tri et un commerce
+    public Contrat(int idContrat, int idCommerce, LocalDateTime debut, LocalDateTime fin, String regles) {
+        this.idContrat = idContrat;
+        this.dateDebut = debut;
+        this.dateFin = fin;
+        this.reglesUtilisation = regles;
     }
 
-    /**
-     * @return
-     */
-    public boolean estActif() {
-        // TODO implement here
+    // Getters et Setters
+    public int getIdContrat() {
+        return idContrat;
+    }
+
+    public void setIdContrat(int idContrat) {
+        this.idContrat = idContrat;
+    }
+
+    public String getReglesUtilisation() {
+        return reglesUtilisation;
+    }
+
+    public String getConditionUtilisation() {
+        return reglesUtilisation;
+    }
+
+    public LocalDateTime getDateDebut() {
+        return dateDebut;
+    }
+
+    public void setDateDebut(LocalDateTime dateDebut) {
+        this.dateDebut = dateDebut;
+    }
+
+    public LocalDateTime getDateFin() {
+        return dateFin;
+    }
+
+    public void setDateFin(LocalDateTime dateFin) {
+        this.dateFin = dateFin;
+    }
+
+    public void setReglesUtilisation(String reglesUtilisation) {
+        this.reglesUtilisation = reglesUtilisation;
+    }
+
+    public int getIdCommerce() {
+        return idCommerce;
+    }
+
+    public void setIdCommerce(int idCommerce) {
+        this.idCommerce = idCommerce;
+    }
+
+    // Renouvelle la fin du contrat avec une nouvelle date
+    public boolean renouvelerContrat(LocalDateTime nouvelleDateFin) {
+        if (nouvelleDateFin.isAfter(this.dateFin)) {
+            this.dateFin = nouvelleDateFin;
+            System.out.println("\nLa date a bien été modifiée !");
+            return true;
+        }
+        System.out.println("\nLa nouvelle date n'est pas valide !");
         return false;
     }
 
-    /**
-     * @return
-     */
-    public string getConditionUtilisation() {
-        // TODO implement here
-        return null;
+    // Vérifie si le contrat est toujours actif
+    public boolean estActif() {
+        LocalDateTime maintenant = LocalDateTime.now();
+        return (dateDebut.isBefore(maintenant) || dateDebut.isEqual(maintenant)) && dateFin.isAfter(maintenant);
     }
-
 }
