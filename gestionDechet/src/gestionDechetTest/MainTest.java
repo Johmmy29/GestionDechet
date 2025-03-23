f813799d0fb49a6caad1c51bc53e9672c67798d8
marzaguina <133380267+marzaguina@users.noreply.github.com>
2025-03-23 18:27:42 +0100

    Add files via upload

---
+++ gestionDechet/src/gestionDechetTest/MainTest.java
@@ -0,0 +1,14 @@
+package gestionDechetTest;
+
+public class MainTest {
+	public static void main(String[] args) {
+		TestPoubelle.main(null);
+		TestContrat.main(null);
+		TestCommerce.main(null);
+		TestCategorieProduit.main(null);
+		TestProduit.main(null);
+		TestDepot.main(null);
+		TestCompte.main(null);
+		TestCentreDeTri.main(null);
+	}
+}
